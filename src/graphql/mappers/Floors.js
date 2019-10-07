1eaefbf8c5ae8789298661a3c7bd5ee718e59633
jamesbeamie <wafulajames9@gmail.com>
2019-10-07 11:31:53 +0300

    update base branch

--- src/graphql/mappers/Floors.js
+++ src/graphql/mappers/Floors.js
@@ -1,8 +1,8 @@
-const formatFloorData = floor => ({
-  id: floor.id,
-  name: floor.name,
-  office: floor.block.offices.name,
-  block: floor.block.name,
+const formatFloorData = (floor) => ({
+	id: floor.id,
+	name: floor.name,
+	office: floor.block.offices.name,
+	block: floor.block.name
 });
 
 export default formatFloorData;
